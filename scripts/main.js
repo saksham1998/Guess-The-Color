fce3055e7e9e1dae5d26735830d067700f52bb5d
Saksham <sakshammanocha14@gmail.com>
2018-11-11 15:13:43 +0530

    Color Guess

---
+++ scripts/main.js
@@ -0,0 +1,76 @@
+var colorArr = [];
+
+var boxEl = document.getElementsByClassName("box"),i,len;
+var colorEl = document.getElementById('colorHeader');
+var stripEl = document.getElementById('strip');
+var messageEl = document.getElementById('message');
+var newEl = document.getElementById('new');
+
+const colorfill = (len) => {
+	for(i=0;i<len;i++){
+		boxEl[i].style.backgroundColor = colorArr[i]; 
+	}
+}
+
+const colorSolve = (len) => {
+	let num = Math.floor(Math.random()*len);
+	colorEl.innerHTML = colorArr[num];
+}
+
+const display = (toShow) => {
+	colorEl.style.color = 'silver'
+	messageEl.innerHTML = '';
+	newEl.innerHTML = `New Colors`
+	if(toShow<=3){
+		var chances = 1;
+		messageEl.innerHTML = `Chances : ${chances}`
+		for(i=3;i<6;i++)
+		boxEl[i].style.display = 'none';
+		len=3;
+	}else{
+		 chances = 3;
+		 len=6;
+		 messageEl.innerHTML = `Chances : ${chances}`;
+		for(i=0;i<toShow;i++){
+			boxEl[i].style.display = 'block'
+			boxEl[i].onclick = function(){
+				chances--;
+				newEl.innerHTML = `Play Again?`
+				if(this.style.backgroundColor === colorEl.innerHTML){
+					for(i=0;i<toShow;i++){
+						boxEl[i].style.backgroundColor = colorEl.innerHTML;
+					}
+				messageEl.innerHTML = `You Won!`;
+				colorEl.style.color = colorEl.innerHTML	
+				}
+				else if(chances>0){
+					 messageEl.innerHTML = `Chances : ${chances}`
+					this.style.backgroundColor = '#232323';
+				}
+				else{
+					messageEl.innerHTML = `You Lose, Try Again!!`;
+					messageEl.style.margin = '0 6%';
+				}
+			}
+		}
+	}	
+}
+
+display(6);
+colorfill(6);
+colorSolve(6);	
+
+const  colorgenerator = (total = (len>3)?6:3) => {
+	colorArr = [];
+	for(i=0;i<total;i++){
+		var num1  = Math.floor(Math.random()*256);
+		var num2  = Math.floor(Math.random()*256);
+		var num3  = Math.floor(Math.random()*256);
+		var color = `rgb(${num1}, ${num2}, ${num3})`;
+		colorArr.push(color);
+	}
+	colorfill(total);
+	colorSolve(total);	 
+	display(total);
+}
+
