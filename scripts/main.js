f1116bcd88e697b6cb58c26675254904a5e061cf
Saksham <sakshammanocha14@gmail.com>
2018-11-13 12:35:38 +0530

    improved logic

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,28 @@ var stripEl = document.getElementById('strip');
 var messageEl = document.getElementById('message');
 var newEl = document.getElementById('new');
 
+const display = (toShow) => {
+	colorEl.style.color = 'silver';
+	messageEl.innerHTML = '';
+	newEl.innerHTML = `New Colors`;
+	messageEl.style.margin = '0 8%';
+	newEl.style.marginLeft = '32.33%';
+	if(toShow==3){
+		var chances = 1;
+		len=3;
+		messageEl.innerHTML = `Chances : ${chances}`
+		for(i=3;i<6;i++){
+			boxEl[i].style.display = 'none';
+		}
+		guess(3,chances);
+	}else{
+		 chances = 3;
+		 len=6;
+		 messageEl.innerHTML = `Chances : ${chances}`;
+		 guess(6,chances);	
+	}	
+}
+
 const colorfill = (len) => {
 	for(i=0;i<len;i++){
 		boxEl[i].style.backgroundColor = colorArr[i]; 
@@ -16,46 +38,7 @@ const colorSolve = (len) => {
 	let num = Math.floor(Math.random()*len);
 	colorEl.innerHTML = colorArr[num];
 }
-
-const display = (toShow) => {
-	colorEl.style.color = 'silver'
-	messageEl.innerHTML = '';
-	newEl.innerHTML = `New Colors`
-	if(toShow<=3){
-		var chances = 1;
-		messageEl.innerHTML = `Chances : ${chances}`
-		for(i=3;i<6;i++)
-		boxEl[i].style.display = 'none';
-		len=3;
-	}else{
-		 chances = 3;
-		 len=6;
-		 messageEl.innerHTML = `Chances : ${chances}`;
-		for(i=0;i<toShow;i++){
-			boxEl[i].style.display = 'block'
-			boxEl[i].onclick = function(){
-				chances--;
-				newEl.innerHTML = `Play Again?`
-				if(this.style.backgroundColor === colorEl.innerHTML){
-					for(i=0;i<toShow;i++){
-						boxEl[i].style.backgroundColor = colorEl.innerHTML;
-					}
-				messageEl.innerHTML = `You Won!`;
-				colorEl.style.color = colorEl.innerHTML	
-				}
-				else if(chances>0){
-					 messageEl.innerHTML = `Chances : ${chances}`
-					this.style.backgroundColor = '#232323';
-				}
-				else{
-					messageEl.innerHTML = `You Lose, Try Again!!`;
-					messageEl.style.margin = '0 6%';
-				}
-			}
-		}
-	}	
-}
-
+	
 display(6);
 colorfill(6);
 colorSolve(6);	
@@ -74,3 +57,30 @@ const  colorgenerator = (total = (len>3)?6:3) => {
 	display(total);
 }
 
+function guess(visible,chances){
+	for(i=0;i<visible;i++){
+		boxEl[i].style.display = 'block';
+		boxEl[i].onclick = function(){
+			newEl.innerHTML = `Play Again?`;
+			chances--;
+			if(this.style.backgroundColor === colorEl.innerHTML){
+				for(i=0;i<visible;i++){
+					boxEl[i].style.backgroundColor = colorEl.innerHTML;
+				}
+				messageEl.innerHTML = `You Won!`;
+				colorEl.style.color = colorEl.innerHTML	
+				}
+			else if(chances>0){
+				 messageEl.innerHTML = `Chances : ${chances}`
+				 this.style.backgroundColor = '#232323';
+			}
+			else{
+				messageEl.innerHTML = `You Lose, Try Again!!`;
+				colorEl.style.color = colorEl.innerHTML;
+				colorEl.innerHTML = `<sub style="font-size:13px;">Color was</sub> ${colorEl.innerHTML}`;
+				messageEl.style.margin = '0 6%';
+				newEl.style.marginLeft = '30%';
+			}
+		}
+	}
+} 
